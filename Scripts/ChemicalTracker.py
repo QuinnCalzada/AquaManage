805f363d11b644b12c8bd966566396e7efd74b7b
Kristen <kristenvigna@gmail.com>
2023-10-04 16:14:02 -0400

    Created ChemicalTracker.py which asks for user input and stores into an array of the user's chemicals

---
+++ Scripts/ChemicalTracker.py
@@ -0,0 +1,65 @@
+#Kristen Vigna
+#Aquamanage
+#October 02, 2023
+#Create an array that stores user inputed information
+
+#set up for Columns: Chemical Name, Low Value, High Value, Current Value
+col = 4
+print(col)
+
+#Rows give the number of chemicals being tracked
+while True:
+    try:
+        rows = int(input('How many Chemicals would you like to add? '))
+    except ValueError:
+        print("Sorry, I didn't understand that.")
+        #better try again... Return to the start of the loop
+        continue
+    else:
+        #rows was successfully parsed!
+        #we're ready to exit the loop.
+        break
+#print(rows)
+
+#Create Array
+Chemicals = [[0] * col for i in range(rows)]
+
+for i in range(rows):
+# loop will run for the length of the inner lists
+    Chemicals[i][0] = input("What is the name of the chemical? ")
+   
+   #Test user input for proper values
+    while True:
+        try:
+            Chemicals[i][1] = float(input("What is the lower limit for this chemical? "))
+        except ValueError:
+            print("Sorry, I didn't understand. Please try again.")
+            continue
+        else:
+            #input was valid
+            break
+
+   #Test user input for proper values
+    while True:
+        try:
+            Chemicals[i][2] = float(input("What is the upper limit for this chemical? "))
+        except ValueError:
+            print("Sorry, I didn't understand. Please try again.")
+            continue
+        else:
+            #input was valid
+            break
+
+   #Test user input for proper values
+    while True:
+        try:
+            Chemicals[i][3] = float(input("What is the current value for this chemical? "))
+        except ValueError:
+            print("Sorry, I didn't understand. Please try again.")
+            continue
+        else:
+            #input was valid
+            break
+    i+1
+
+print(Chemicals)
