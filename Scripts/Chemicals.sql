8e76652958bfd949c2b4c3dd25a8c4b11c4d5317
Elise Dezotell <dezotele@my.erau.edu>
2023-10-18 20:56:05 -0400

    trying to get database working so chemicals can work

---
+++ Scripts/Chemicals.sql
@@ -0,0 +1,1 @@
+CREATE DATABASE chemical_data;
--- templates/ChemicalTracker.py
+++ templates/ChemicalTracker.py
@@ -6,28 +6,34 @@
 #Create an array that stores user inputed information
 
 from flask import Flask, render_template, request, redirect, url_for
-import sqlite3
+import mysql.connector
 
-app = Flask('__main__')
+app = Flask(__name__)
+
+# Define the database connection
+db = mysql.connector.connect(
+    host="AquaManage",
+    user="root",
+    password="Doodles7",
+    database="chemical_data"
+)
 
 @app.route('/')
 def index():
-    connection = sqlite3.connect("database.db")
-    cursor = connection.cursor()
-    cursor.execute("CREATE spreadsheet")
-    cursor.execute("SELECT * FROM spreadsheet")
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM chemical_data")
     data = cursor.fetchall()
-    connection.close()
+    cursor.close()
     return render_template('index.html', data=data)
 
-@app.route('/save', methods=['POST'])
-def save():
-    data = request.form.getlist('data')
-    connection = sqlite3.connect("database.db")
-    cursor = connection.cursor()
-    cursor.execute("INSERT INTO spreadsheet (column1, column2, column3, column4, column5) VALUES (?, ?, ?, ?, ?)", data)
-    connection.commit()
-    connection.close()
+@app.route('/add', methods=['POST'])
+def add():
+    name = request.form['name']
+    value = float(request.form['value'])
+    cursor = db.cursor()
+    cursor.execute("INSERT INTO chemical_data (name, value) VALUES (%s, %s)", (name, value))
+    db.commit()
+    cursor.close()
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
